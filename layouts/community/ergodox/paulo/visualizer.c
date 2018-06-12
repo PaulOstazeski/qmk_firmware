66df25af1dff796e7c8082d461c532e5e5044f9a
Paul Ostazeski <paul.ostazeski@gmail.com>
2018-06-12 18:53:21 -0400

    Named colors

--- layouts/community/ergodox/paulo/visualizer.c
+++ layouts/community/ergodox/paulo/visualizer.c
@@ -12,6 +12,19 @@ enum custom_layers {
     _MOUSE,
 };
 
+#define RED 0
+#define ORANGE 21
+#define YELLOW 42
+#define SPRING_GREEN 64
+#define GREEN 85
+#define TURQUOISE 107
+#define CYAN 127
+#define OCEAN 149
+#define BLUE 170
+#define VIOLET 192
+#define MAGENTA 212
+#define RASPBERRY 234
+
 // This function should be implemented by the keymap visualizer
 // Don't change anything else than state->target_lcd_color and state->layer_text as that's the only thing
 // that the simple_visualizer assumes that you are updating
@@ -22,15 +35,15 @@ static void get_visualizer_layer_and_color(visualizer_state_t* state) {
 
     switch(layer) {
       case(_QWERTY):
-        state->target_lcd_color = LCD_COLOR(240, 100, 0xFF);
+        state->target_lcd_color = LCD_COLOR(RASPBERRY, 255, 0xFF);
         state->layer_text = "Alpha";
         break;
       case(_NUM):
-        state->target_lcd_color = LCD_COLOR(0, 100, 0xFF);
+        state->target_lcd_color = LCD_COLOR(CYAN, 255, 0xFF);
         state->layer_text = "Symbol";
         break;
       case(_MOUSE):
-        state->target_lcd_color = LCD_COLOR(60, 100, 0xFF);
+        state->target_lcd_color = LCD_COLOR(YELLOW, 255, 0xFF);
         state->layer_text = "Mouse";
         break;
     }
